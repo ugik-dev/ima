fd031e6c3615e624c069adaca60ace58889cbe87
ugik <ugik.dev@gmail.com>
2021-05-04 11:10:17 +0700

    as

--- assets/dist/js/backend/journal_voucher.js
+++ assets/dist/js/backend/journal_voucher.js
@@ -70,6 +70,26 @@ function check_validity() {
   }
 }
 
+function formatRupiah(angka, prefix) {
+  var number_string = angka.toString();
+  split = [];
+  split[0] = number_string.slice(0, -2);
+  split[1] = number_string.slice(-2);
+
+  sisa = split[0].length % 3;
+  (rupiah = split[0].substr(0, sisa)),
+    (ribuan = split[0].substr(sisa).match(/\d{3}/gi));
+
+  // tambahkan titik jika yang di input sudah menjadi angka ribuan
+  if (ribuan) {
+    separator = sisa ? "." : "";
+    rupiah += separator + ribuan.join(".");
+  }
+
+  rupiah = split[1] != undefined ? rupiah + "," + split[1] : rupiah;
+  return prefix == undefined ? rupiah : rupiah ? "Rp. " + rupiah : "";
+}
+
 function delete_row(row) {
   // console.log(row);
 
