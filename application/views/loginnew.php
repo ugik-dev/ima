586424295de82543d8d0467af3f19492ca23c60a
Ugik Dev <ugik.dev@gmail.com>
2023-02-15 02:40:21 +0000

    frm serv

--- application/views/loginnew.php
+++ application/views/loginnew.php
@@ -57,11 +57,11 @@
                                 <input class="form-control h-auto text-white bg-white-o-5 rounded-pill border-0 py-4 px-8" type="password" placeholder="Password" name="password" />
                             </div>
                             <div class="form-group d-flex flex-wrap justify-content-between align-items-center px-8 opacity-60">
-                                <div class="checkbox-inline">
+                               <!-- <div class="checkbox-inline">
                                     <label class="checkbox checkbox-outline checkbox-white text-white m-0">
                                         <input type="checkbox" name="remember" />
                                         <span></span>Remember me</label>
-                                </div>
+                                </div> -->
                                 <a href="javascript:;" id="kt_login_forgot" class="text-white font-weight-bold">Forget Password ?</a>
                             </div>
                             <div class="form-group text-center mt-10">
@@ -71,7 +71,7 @@
                         <!-- <div class="mt-10" style="display: none;">
                             <span class="opacity-40 mr-4">Don't have an account yet?</span>
                             <a href="javascript:;" id="kt_login_signup" class="text-white opacity-30 font-weight-normal">Sign Up</a>
-                        </div> -->
+                        </div>-->
                     </div>
                     <!--end::Login Sign in form-->
                     <!--begin::Login Sign up form-->
