dc34dbcfd05974df09cc38b7079af6cdefd98f2f
Ugik Dev <ugik.dev@gmail.com>
2023-02-18 12:19:28 +0700

    sa

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -53,7 +53,7 @@ class Dashboard extends CI_Controller
 		// DEFINES FETCH THE productS RECORD FROM TABLE mp_productslist WITH LIMIT OF ONLY 6 RECORD
 		$data['productList_records'] = $this->Crud_model->fetch_limit_record('mp_productslist', 6);
 		echo "<br>52";
-		var_dump($data['productList_records']);
+		// var_dump($data['productList_records']);
 
 		// DEFINES FETCH THE CUSTOMER RECORD FROM TABLE MP_CUSTOMER WITH LIMIT OF ONLY 8 RECORD
 		$data['total_retial_cost'] = $this->Crud_model->result_retail_cost();
@@ -68,7 +68,7 @@ class Dashboard extends CI_Controller
 		//CASH IN HAND
 		$data['cash_in_hand'] = $this->Statement_model->count_current_time(array('acc_number' => '1.11.000.000'));
 		echo "<br>70<br>";
-		var_dump($data);
+		// var_dump($data);
 
 		//ACCOUNT RECEIVABLE
 		$data['account_recieveble'] = $this->Statement_model->count_current_time(array('acc_number' => '1.13.000.000'));
